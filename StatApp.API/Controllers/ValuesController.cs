d69f90da2c34eae18ec123256d698d5e7dbf7249
Omar Nabolsi <omar.nabolsi@gmail.com>
2019-09-11 14:51:07 +0300

    End of Section 3

--- StatApp.API/Controllers/ValuesController.cs
+++ StatApp.API/Controllers/ValuesController.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using StatApp.API.Data;
 
 namespace StatApp.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -27,6 +29,7 @@ namespace StatApp.API.Controllers
         }
 
         // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
