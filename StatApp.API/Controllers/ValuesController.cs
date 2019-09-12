34ccccd0fe0ffb12b28405a65923e5cde2d3cfcc
Omar Nabolsi <omar.nabolsi@gmail.com>
2019-09-12 09:15:02 +0300

    End of Section 4

--- StatApp.API/Controllers/ValuesController.cs
+++ StatApp.API/Controllers/ValuesController.cs
@@ -20,6 +20,7 @@ namespace StatApp.API.Controllers
         }
 
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
